a34ede0873281d8883f5dcc0349212e09985fd39
Danik1702 <dkichalo@gmail.com>
2023-03-08 14:54:48 +0200

    set proper link urls

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,7 @@ const  App = () => {
             <AboutUs
               text="About Us"
               description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eleifend tincidunt posuere eu risus cursus. Senectus sed maecenas viverra tempus at. Nulla aliquam ac diam amet, sagittis, ut ultricies. Urna hendrerit morbi sed gravida. Nulla aliquam ac diam amet, sagittis, ut ultricies. Urna hendrerit morbi sed gravida. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eleifend tincidunt posuere eu risus cursus."
-              linkUrl="#"
+              linkUrl="#about-us"
               linkText="Learn more about →"
             />
           </div>
@@ -67,7 +67,7 @@ const  App = () => {
             <AboutUs
               text="About Us 2"
               description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eleifend tincidunt posuere eu risus cursus. Senectus sed maecenas viverra tempus at. Nulla aliquam ac diam amet, sagittis, ut ultricies. Urna hendrerit morbi sed gravida. Nulla aliquam ac diam amet, sagittis, ut ultricies. Urna hendrerit morbi sed gravida. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eleifend tincidunt posuere eu risus cursus."
-              linkUrl="#"
+              linkUrl="#about-us"
               linkText="Learn more about →"
             />
           </div>
